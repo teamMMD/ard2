18015eb6e94cd1e988bdf7a7bc82201bc6a0e0a0
mayamarks <maya.marks.dc@gmail.com>
2020-09-19 18:57:21 -0400

    reading from scores text file functionality added

--- src/com/mwl/screens/ScoreScreen.java
+++ src/com/mwl/screens/ScoreScreen.java
@@ -7,53 +7,35 @@ import java.awt.event.KeyEvent;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class ScoreScreen implements Screen {
     BufferedReader reader;
 
-    public String scoreScreenReadFromTxt() {
-
-        //the following code skips lines <...>
-//        try {
-//            reader = new BufferedReader(new FileReader("resources/scores/final_scores.txt"));
-//            String scores = reader.readLine();
-//            while (scores != null) {
-//                System.out.println(scores);
-//                scores = reader.readLine();
-//                return scores;
-//            }
-//            reader.close();
-//        } catch ( IOException e) {
-//            System.out.println("file not found");
-//            e.printStackTrace();
-//        }
-//        return "no one played yet";
-
-        //String thisLine = null;
-
+    public String scoreScreenReadFromTxt(int x) {
         try {
-            // open input stream test.txt for reading purpose.
-            BufferedReader br = new BufferedReader(new FileReader("resources/scores/final_scores.txt"));
+            if (Files.readAllLines(Paths.get("resources/scores/final_scores.txt")).size() >= x
+            && Files.readAllLines(Paths.get("resources/scores/final_scores.txt")).get(x).length() > 0) {
+                String line = Files.readAllLines(Paths.get("resources/scores/final_scores.txt")).get(x);
+                return line;
+            }
 
-//            while ((thisLine = br.readLine()) != null) {
-//                return thisLine;
-//            }
-                String line;
-                while ((line = br.readLine()) != null) {
-                    // process the line.
-                    return line;
-                }
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (IOException e) {
+            //e.printStackTrace();
+            return "no scores";
         }
         return "no scores";
     }
 
     @Override
     public void displayOutput(AsciiPanel terminal) {
-        terminal.writeCenter(this.scoreScreenReadFromTxt(), 5);
-        //TODO: we want to be able print the text to multiple lines.
-        // the above code limits the output to only one line #5
+        terminal.writeCenter(this.scoreScreenReadFromTxt(3), 5);
+        terminal.writeCenter(this.scoreScreenReadFromTxt(5), 6);
+        terminal.writeCenter(this.scoreScreenReadFromTxt(7), 7);
+        terminal.writeCenter(this.scoreScreenReadFromTxt(9), 8);
+        terminal.writeCenter(this.scoreScreenReadFromTxt(11), 9);
+        terminal.writeCenter(this.scoreScreenReadFromTxt(13), 10);
 
     }
 
